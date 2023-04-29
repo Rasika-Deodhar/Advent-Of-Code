5ab3541097e993ea71e26b1f803700c874d0be39
Rasika Deodhar <rasikadeodhar95@gmail.com>
2023-04-29 23:56:27 +0100

    day 3 part 1 solved

---
+++ 2022_Day3/2022_Day3.py
@@ -0,0 +1,30 @@
+f = open("input.txt", "r")
+input = f.readlines()
+
+
+# thanks to chat-gpt
+letter_to_number = {chr(i): i - 96 for i in range(97, 123)}
+letter_to_number.update({chr(i): i - 38 for i in range(65, 91)})
+
+# letter_to_number = {chr(i): i - 96 if i <= 122 else i - 38 for i in range(65, 91+26)}
+
+
+print('letters', letter_to_number)
+
+res = 0
+
+for line in input:
+    length = len(line)
+    half = int(length/2)
+    compartment1 = line[0:half]
+    compartment2 = line[half : length]
+    matches = []
+    print(line + '----' + compartment1 + '---' + compartment2)
+    for i in compartment1:
+        if i in compartment2:
+            matches.append({'letter': i, 'score': letter_to_number[i]})
+            compartment2 = compartment2.replace(i, '')
+    print('matches = ', matches)
+    res += sum([m['score'] for m in matches])
+
+print('sum of matches = ', res)
