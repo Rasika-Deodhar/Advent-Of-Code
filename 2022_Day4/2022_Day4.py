031ae81ab9fc290f450fe469371ac3fd96d4837f
Rasika Deodhar <rasikadeodhar95@gmail.com>
2023-05-03 23:55:55 +0100

    day 4 part 1

---
+++ 2022_Day4/2022_Day4.py
@@ -0,0 +1,29 @@
+f = open("input.txt", "r")
+input = f.readlines()
+
+overlap=0
+
+for line in input:
+    first = line.split(',')[0]
+    second = line.split(',')[1]
+
+    first_0 = first.split('-')[0]
+    first_1 = first.split('-')[1]    
+    
+    second_0 = second.split('-')[0]
+    second_1 = second.split('-')[1]
+
+    
+    print(first_0, first_1, second_0, second_1)
+
+    if int(first_0)==2 and int(first_1)==69:
+        print('hey')
+
+    if int(first_0) <= int(second_0) and int(second_1) <= int(first_1):
+            overlap += 1
+            print('added')
+    elif int(second_0)  <= int(first_0) and int(second_1) >= int(first_1):
+            overlap +=1
+            print('added')
+
+print('overlap =', overlap)
